d9b4725c2a10596629b951f63d0f7fe727f2f8f7
Jerry Ng <jerryng500@hotmail.com>
2020-03-31 10:56:33 -0400

    Finalized process and got rid of excess images

--- scripts/process.py
+++ scripts/process.py
@@ -1,14 +1,9 @@
 import cv2
+import numpy as np
 
-img_lena=cv2.imread('Example_lena_denoise_noisy.jpg',1)
-img_flower=cv2.imread('Flower_noisy.jpg',1)
-cv2.imshow('image',img_lena)
-cv_image=cv2.imread('Flower_noisy.jpg',1)
-##
-##cv2.waitKey(0)
-##cv2.destroyAllWindows()
-##
-##cv2.imwrite('lena_copy.png',img_lena)
+
+cv_image=cv2.imread('sudoku.png',1)
+
 cv2.imshow("Original_Image", cv_image)
 cv2.waitKey(3) 
     
@@ -21,12 +16,10 @@ edges = cv2.Canny(gray,10,500,apertureSize = 3)   # Canny edge detector to make 
 cv2.imshow("Canny_Image", edges)
 cv2.waitKey(3) 
 
-min_intersections = 200                           # TO DO: Play with this parameter to change sensitivity.
+min_intersections = 80                          # TO DO: Play with this parameter to change sensitivity.
 lines = cv2.HoughLines(edges,1,np.pi/180,min_intersections)     # Run Hough Transform
-num_lines = 0;
-shape = lines.shape
-for i in range(shape[0]):                         # Plot lines over original feed
-    for rho,theta in lines[i]:
+for line in lines:                         # Plot lines over original feed
+    for rho,theta in line:
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a*rho
@@ -36,8 +29,7 @@ for i in range(shape[0]):                         # Plot lines over original fee
         x2 = int(x0 - 1000*(-b))
         y2 = int(y0 - 1000*(a))
         cv2.line(cv_image,(x1,y1),(x2,y2),(0,0,255),2)
-        num_lines += 1
         
 cv2.imshow("Line_Detected_Image", cv_image)
-cv2.waitKey(5)
+cv2.waitKey(0)
 print("Detecting Lines...")
